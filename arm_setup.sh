d0ff35f2d469ec533ac74dbaa3007e6d14442367
Hao Li <hl521@duke.edu>
2026-05-21 14:28:46 -0400

    fixed arm_setup.sh

--- arm_setup.sh
+++ arm_setup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+
 # 1. Define variables
 REPO="hli521/homeKaraoke"
 FILE_NAME="ggml-small.tar.gz"
 TARGET_DIR="./resources/whisper/darwin-arm64"
 
+
+# curl -s "https://api.github.com/repos/$REPO/releases" | grep browser_download_url
 echo "==> Fetching latest release download URL..."
-URL=$(curl -s "https://github.com" | grep "browser_download_url" | grep "$FILE" | cut -d '"' -f 4)
+
+URL=$(curl -s "https://api.github.com/repos/$REPO/releases" \
+  | grep "browser_download_url" \
+  | grep "$FILE_NAME" \
+  | cut -d '"' -f 4)
 
 if [ -z "$URL" ]; then
-    echo "Error: Could not find $FILE in the latest release."
+    echo "Error: Could not find $FILE_NAME in the latest release."
     exit 1
 fi
 
 echo "==> Downloading and extracting binary directly to $TARGET_DIR..."
-# Downloads and decompresses on-the-fly without saving the .tar.gz to disk
-curl -L "$URL" | tar -xzvf - -C "$TARGET_DIR"
+
+curl -L "$URL" | tar -xzvf - -C "."
 
 echo "==> Setup complete! Binary is in $TARGET_DIR"
